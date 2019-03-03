e6def60a123b20caf385a295f9c78da15efdd646
aaronhannon <aaronchannon98@gmail.com>
2019-03-03 19:39:44 +0000

    Some Match settings added

--- MobileApps2Project/MobileApps2Project/MatchSettings.xaml.cs
+++ MobileApps2Project/MobileApps2Project/MatchSettings.xaml.cs
@@ -15,6 +15,44 @@ namespace MobileApps2Project
 		public MatchSettings ()
 		{
 			InitializeComponent ();
-		}
+
+            NavigationPage.SetHasNavigationBar(this, false);
+
+            toolbarStk.BackgroundColor = Color.FromHex("#3f7247");
+            toolbarStk.HeightRequest = 55;
+            toolbarStk.HorizontalOptions = LayoutOptions.FillAndExpand;
+
+            bar.Text = "Match Settings";
+            bar.HorizontalTextAlignment = TextAlignment.Start;
+            bar.Margin = 15;
+            bar.TextColor = Color.White;
+            bar.FontSize = 18;
+            bar.FontAttributes = FontAttributes.Bold;
+
+            settingStk.Padding = 10;
+
+            Entry input1 = new Entry()
+            {
+                Placeholder = "Player 1",
+                WidthRequest = 200,
+                HeightRequest = 35,
+                HorizontalOptions = LayoutOptions.Center,
+                VerticalOptions = LayoutOptions.Center
+            };
+
+            Entry input2 = new Entry()
+            {
+                Placeholder = "Player 2",
+                WidthRequest = 200,
+                HeightRequest = 35,
+                HorizontalOptions = LayoutOptions.Center,
+                VerticalOptions = LayoutOptions.Center
+            };
+            settingStk.Children.Add(input1);
+            settingStk.Children.Add(input2);
+
+            
+
+        }
 	}
 }
